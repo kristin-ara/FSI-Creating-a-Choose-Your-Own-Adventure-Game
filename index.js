6371aec45bdfcdf12688c757e1f2868b96d4d3ab
Kristin Arakawa <kristinarakawa12@gmail.com>
2022-06-17 17:02:25 -1000

    test commit 2

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
 window.prompt('Do you head left or right?')
-let answer = window.prompt('Do you head left or right?')
-let firstAnswer = window.prompt('Do you head left or right?')
+let firstAnswer = window.prompt ('do you head left or right?')
 console.log(firstAnswer)
-let firstAnswer= window.prompt('Do you head left or right?')
-console.log(firstAnswer ==='left')
+console.log(firstAnswer === 'left')
+if(firstAnswer === 'left'){
+    let secondAnswer = window.prompt(`You come across a stray cat. 
+It scampers off down a small hole, just large enough for you to crawl through. 
+Do you follow it, or continue on your path?`)
+
+} else if(firstAnswer === 'right') {
+    let secondAnswer = window.prompt(`You come across a snoring dragon. 
+On the other side of him, you see a shiny chest of treasure. Another path would 
+lead you away from the dragon altogether. Which path do you take?`)
+
+}
+window.prompt('Do you follow or continue?')
+let secondAnswer = window.prompt ('Do you follow or continue?')
+console.log(secondAnswer)
+console.log(secondAnswer === 'follow')
+if(firstAnswer === 'follow'){
+    let thirdAnswer = window.prompt(`You come across a stray cat. 
+It scampers off down a small hole, just large enough for you to crawl through. 
+Do you follow it, or continue on your path?`)
+
+} else if(secondAnswer === 'continue') {
+    let thirdAnswer = window.prompt(`You come across a snoring dragon. 
+On the other side of him, you see a shiny chest of treasure. Another path would 
+lead you away from the dragon altogether. Which path do you take?`)
+
+}
