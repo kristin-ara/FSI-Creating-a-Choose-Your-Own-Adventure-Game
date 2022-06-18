16e8c466256bdbb801470d55e7884a90ea30454d
Kristin Arakawa <kristinarakawa12@gmail.com>
2022-06-17 14:42:30 -1000

    test commit

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-//Your Code here
+window.prompt('Do you head left or right?')
+let answer = window.prompt('Do you head left or right?')
+let firstAnswer = window.prompt('Do you head left or right?')
+console.log(firstAnswer)
+let firstAnswer= window.prompt('Do you head left or right?')
+console.log(firstAnswer ==='left')
