13c130efd704810ecfefc2a74e5aec04676e5d78
Kristin Arakawa <kristinarakawa12@gmail.com>
2022-06-20 14:57:34 -1000

    almost done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-window.prompt('Do you head left or right?')
+
 let firstAnswer = window.prompt ('do you head left or right?')
-console.log(firstAnswer)
 console.log(firstAnswer === 'left')
+
 if(firstAnswer === 'left'){
     let secondAnswer = window.prompt(`You come across a stray cat. 
 It scampers off down a small hole, just large enough for you to crawl through. 
@@ -13,18 +13,19 @@ On the other side of him, you see a shiny chest of treasure. Another path would
 lead you away from the dragon altogether. Which path do you take?`)
 
 }
-window.prompt('Do you follow or continue?')
-let secondAnswer = window.prompt ('Do you follow or continue?')
-console.log(secondAnswer)
+let secondAnswer = ('follow')
 console.log(secondAnswer === 'follow')
-if(firstAnswer === 'follow'){
-    let thirdAnswer = window.prompt(`You come across a stray cat. 
-It scampers off down a small hole, just large enough for you to crawl through. 
-Do you follow it, or continue on your path?`)
+if(secondAnswer === 'follow'){
+    let thirdAnswer = window.prompt(`You follow the cat to a colony of cats, nestled in a fort of warm blankets and subsisting off of inexplicably warm soup. They are content with you staying, but you wonder if you should alert the world to this magical safe haven.`)
 
 } else if(secondAnswer === 'continue') {
-    let thirdAnswer = window.prompt(`You come across a snoring dragon. 
-On the other side of him, you see a shiny chest of treasure. Another path would 
-lead you away from the dragon altogether. Which path do you take?`)
+    let thirdAnswer = window.prompt(`You come across a chamber that reaches upward to a shining light above. There is a long, winding staircase, and a much quicker, but rickety-looking ladder that leads up toward the light. Which do you take?`)
+}
 
+let thirdAnswer = ('stay')
+console.log(thirdAnswer === 'stay')
+if(thirdAnswer === 'stay'){
+    let fourthAnswer = window.alert(`You live happily amongst the cats for the rest of your days.`)
+} else if(thirdAnswer === 'leave') {
+    let fourthAnswer = window.alert(`After leaving the cat colony, you are never able to find it again; without proof, no one believes your story, which passes into legend nonetheless.`)
 }
